82ca50ab57d5b13b2fc0957216246b575cd6a320
minbou1201 <85299867+minbou1201@users.noreply.github.com>
2021-06-03 22:03:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'yêu đương cc :))',
     mess: 'Anh biết mà. Yêu em nhiều nhiều',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
